a4efbfdd14b806dc0086f5ec33210c999a257c4e
catelynbu <catelyn.bu@gmail.com>
2025-08-22 17:59:16 -0700

    adding images

--- projects/goodnotes/goodnotes.js
+++ projects/goodnotes/goodnotes.js
@@ -1,259 +1,134 @@
-// (function(){
-//     "use strict";
-//     console.log('this is chevron script');
+(function(){
+    "use strict";
+    console.log('this is goodnotes script');
 
+    const canvasBtn = document.querySelector('#canvas');
+    const promptBtn = document.querySelector('#prompt');
+    const acceptBtn = document.querySelector('#accept');
+    const quickBtn = document.querySelector('#quick');
+    const suggestionsBtn = document.querySelector('#suggestions');
 
-//     const lowfiBtn = document.querySelector('#lowfi');
-//     const midfiBtn = document.querySelector('#midfi');
+    const canvasContent = document.querySelector('#canvas-content');
+    const promptContent = document.querySelector('#prompt-content');
+    const acceptContent = document.querySelector('#accept-content');
+    const quickContent = document.querySelector('#quick-content');
+    const suggestionsContent = document.querySelector('#suggestions-content');
 
-//     const lowfiContent = document.querySelector('#lowfi-content');
-//     const midfiContent = document.querySelector('#midfi-content');
+    canvasBtn.addEventListener('click', function(){
+        console.log('i wanna see canvas stuff')
 
-//     lowfiBtn.addEventListener('click', function(){
-//         //button interactions
-//         lowfiBtn.classList.add('selected-button');
-//         midfiBtn.classList.remove('selected-button');
+        //button interactions
+        canvasBtn.classList.add('selected-button');
 
-        
-//         //content displayment
-//         lowfiContent.style.display = 'block';
-//         midfiContent.style.display = 'none';
-//     })
-
-//     midfiBtn.addEventListener('click', function(){
-//         //button interactions
-//         midfiBtn.classList.add('selected-button');
-//         lowfiBtn.classList.remove('selected-button');
-
-        
-//         //content displayment
-//         midfiContent.style.display = 'block';
-//         lowfiContent.style.display = 'none';
-//     })
-
-
-
-
-
-
-//     const goalBtn = document.querySelector('#goal');
-//     const competitiveBtn = document.querySelector('#competitive');
-//     const interviewBtn = document.querySelector('#interview');
-//     const affinityBtn = document.querySelector('#affinity');
-
-//     const goalContent = document.querySelector('#goal-content');
-//     const competitiveContent = document.querySelector('#competitive-content');
-//     const interviewContent = document.querySelector('#interview-content');
-//     const affinityContent = document.querySelector('#affinity-content');
-
-//     goalBtn.addEventListener('click', function(){
-//         console.log('i wanna see goal stuff')
-
-//         //button interactions
-//         goalBtn.classList.add('selected-button');
-
-//         competitiveBtn.classList.remove('selected-button');
-//         interviewBtn.classList.remove('selected-button');
-//         affinityBtn.classList.remove('selected-button');
+        promptBtn.classList.remove('selected-button');
+        acceptBtn.classList.remove('selected-button');
+        quickBtn.classList.remove('selected-button');
+        suggestionsBtn.classList.remove('selected-button');
 
         
-//         //content displayment
-//         goalContent.style.display = 'block';
+        //content displayment
+        canvasContent.style.display = 'block';
 
-//         competitiveContent.style.display = 'none';
-//         interviewContent.style.display = 'none';
-//         affinityContent.style.display = 'none';
-//     })
+        promptContent.style.display = 'none';
+        acceptContent.style.display = 'none';
+        quickContent.style.display = 'none';
+        suggestionsContent.style.display = 'none';
+    })
 
-//     competitiveBtn.addEventListener('click', function(){
-//         console.log('i wanna see goal stuff')
 
-//         //button interactions
-//         competitiveBtn.classList.add('selected-button');
 
-//         goalBtn.classList.remove('selected-button');
-//         interviewBtn.classList.remove('selected-button');
-//         affinityBtn.classList.remove('selected-button');
+    promptBtn.addEventListener('click', function(){
+        console.log('i wanna see prompt stuff')
 
+        //button interactions
+        promptBtn.classList.add('selected-button');
+
+        canvasBtn.classList.remove('selected-button');
+        acceptBtn.classList.remove('selected-button');
+        quickBtn.classList.remove('selected-button');
+        suggestionsBtn.classList.remove('selected-button');
+
+
+        //content displayment
+        promptContent.style.display = 'block';
+
+        canvasContent.style.display = 'none';
+        acceptContent.style.display = 'none';
+        quickContent.style.display = 'none';
+        suggestionsContent.style.display = 'none';
         
-//         //content displayment
-//         competitiveContent.style.display = 'block';
+    })
 
-//         goalContent.style.display = 'none';
-//         interviewContent.style.display = 'none';
-//         affinityContent.style.display = 'none';
-//     })
 
-//     interviewBtn.addEventListener('click', function(){
-//         console.log('i wanna see goal stuff')
 
-//         //button interactions
-//         interviewBtn.classList.add('selected-button');
+    acceptBtn.addEventListener('click', function(){
+        console.log('i wanna see accept stuff')
 
-//         goalBtn.classList.remove('selected-button');
-//         competitiveBtn.classList.remove('selected-button');
-//         affinityBtn.classList.remove('selected-button');
+        //button interactions
+        acceptBtn.classList.add('selected-button');
 
+        canvasBtn.classList.remove('selected-button');
+        promptBtn.classList.remove('selected-button');
+        quickBtn.classList.remove('selected-button');
+        suggestionsBtn.classList.remove('selected-button');
+
+
+        //content displayment
+        acceptContent.style.display = 'block';
+
+        canvasContent.style.display = 'none';
+        promptContent.style.display = 'none';
+        quickContent.style.display = 'none';
+        suggestionsContent.style.display = 'none';
+    })
+
+
+
+    quickBtn.addEventListener('click', function(){
+        console.log('i wanna see quick stuff')
         
-//         //content displayment
-//         interviewContent.style.display = 'block';
+        //button interactions
+        quickBtn.classList.add('selected-button');
 
-//         goalContent.style.display = 'none';
-//         competitiveContent.style.display = 'none';
-//         affinityContent.style.display = 'none';
-//     })
+        canvasBtn.classList.remove('selected-button');
+        promptBtn.classList.remove('selected-button');
+        acceptBtn.classList.remove('selected-button');
+        suggestionsBtn.classList.remove('selected-button');
 
-//     affinityBtn.addEventListener('click', function(){
-//         console.log('i wanna see goal stuff')
 
-//         //button interactions
-//         affinityBtn.classList.add('selected-button');
+        //content displayment
+        quickContent.style.display = 'block';
 
-//         goalBtn.classList.remove('selected-button');
-//         competitiveBtn.classList.remove('selected-button');
-//         interviewBtn.classList.remove('selected-button');
+        canvasContent.style.display = 'none';
+        promptContent.style.display = 'none';
+        acceptContent.style.display = 'none';
+        suggestionsContent.style.display = 'none';
+    })
 
+
+
+    suggestionsBtn.addEventListener('click', function(){
+        console.log('i wanna see suggestions stuff')
         
-//         //content displayment
-//         affinityContent.style.display = 'block';
+        //button interactions
+        suggestionsBtn.classList.add('selected-button');
 
-//         goalContent.style.display = 'none';
-//         competitiveContent.style.display = 'none';
-//         interviewContent.style.display = 'none';
-//     })
-
-
+        canvasBtn.classList.remove('selected-button');
+        promptBtn.classList.remove('selected-button');
+        acceptBtn.classList.remove('selected-button');
+        quickBtn.classList.remove('selected-button');
 
 
+        //content displayment
+        suggestionsContent.style.display = 'block';
 
-
-//     const dashboardBtn = document.querySelector('#dashboard');
-//     const searchBtn = document.querySelector('#search');
-//     const runBtn = document.querySelector('#run');
-//     const previewBtn = document.querySelector('#preview');
-//     const historyBtn = document.querySelector('#history');
-
-//     const dashboardContent = document.querySelector('#dashboard-content');
-//     const searchContent = document.querySelector('#search-content');
-//     const runContent = document.querySelector('#run-content');
-//     const previewContent = document.querySelector('#preview-content');
-//     const historyContent = document.querySelector('#history-content');
-
-//     dashboardBtn.addEventListener('click', function(){
-//         console.log('i wanna see dashboard stuff')
-
-//         //button interactions
-//         dashboardBtn.classList.add('selected-button');
-
-//         searchBtn.classList.remove('selected-button');
-//         runBtn.classList.remove('selected-button');
-//         previewBtn.classList.remove('selected-button');
-//         historyBtn.classList.remove('selected-button');
-
-        
-//         //content displayment
-//         dashboardContent.style.display = 'block';
-
-//         searchContent.style.display = 'none';
-//         runContent.style.display = 'none';
-//         previewContent.style.display = 'none';
-//         historyContent.style.display = 'none';
-//     })
-
-
-
-//     searchBtn.addEventListener('click', function(){
-//         console.log('i wanna see search stuff')
-
-//         //button interactions
-//         searchBtn.classList.add('selected-button');
-
-//         dashboardBtn.classList.remove('selected-button');
-//         runBtn.classList.remove('selected-button');
-//         previewBtn.classList.remove('selected-button');
-//         historyBtn.classList.remove('selected-button');
-
-
-//         //content displayment
-//         searchContent.style.display = 'block';
-
-//         dashboardContent.style.display = 'none';
-//         runContent.style.display = 'none';
-//         previewContent.style.display = 'none';
-//         historyContent.style.display = 'none';
-        
-//     })
-
-
-
-//     runBtn.addEventListener('click', function(){
-//         console.log('i wanna see run stuff')
-
-//         //button interactions
-//         runBtn.classList.add('selected-button');
-
-//         dashboardBtn.classList.remove('selected-button');
-//         searchBtn.classList.remove('selected-button');
-//         previewBtn.classList.remove('selected-button');
-//         historyBtn.classList.remove('selected-button');
-
-
-//         //content displayment
-//         runContent.style.display = 'block';
-
-//         dashboardContent.style.display = 'none';
-//         searchContent.style.display = 'none';
-//         previewContent.style.display = 'none';
-//         historyContent.style.display = 'none';
-//     })
-
-
-
-//     previewBtn.addEventListener('click', function(){
-//         console.log('i wanna see preview stuff')
-        
-//         //button interactions
-//         previewBtn.classList.add('selected-button');
-
-//         dashboardBtn.classList.remove('selected-button');
-//         searchBtn.classList.remove('selected-button');
-//         runBtn.classList.remove('selected-button');
-//         historyBtn.classList.remove('selected-button');
-
-
-//         //content displayment
-//         previewContent.style.display = 'block';
-
-//         dashboardContent.style.display = 'none';
-//         searchContent.style.display = 'none';
-//         runContent.style.display = 'none';
-//         historyContent.style.display = 'none';
-//     })
-
-
-
-//     historyBtn.addEventListener('click', function(){
-//         console.log('i wanna see history stuff')
-        
-//         //button interactions
-//         historyBtn.classList.add('selected-button');
-
-//         dashboardBtn.classList.remove('selected-button');
-//         searchBtn.classList.remove('selected-button');
-//         runBtn.classList.remove('selected-button');
-//         previewBtn.classList.remove('selected-button');
-
-
-//         //content displayment
-//         historyContent.style.display = 'block';
-
-//         dashboardContent.style.display = 'none';
-//         searchContent.style.display = 'none';
-//         runContent.style.display = 'none';
-//         previewContent.style.display = 'none';
-//     })
+        canvasContent.style.display = 'none';
+        promptContent.style.display = 'none';
+        acceptContent.style.display = 'none';
+        quickContent.style.display = 'none';
+    })
 
 
 
 
-// })();
+})();
